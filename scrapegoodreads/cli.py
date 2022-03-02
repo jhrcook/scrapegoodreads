a4cc4d269b3ea8db3055d230bc5792c2506b7d01
Josh Cook <joshuacook0023@gmail.com>
2022-03-02 06:42:41 -0500

    feat: let user log-in in browser window, check for successful log-in

--- scrapegoodreads/cli.py
+++ scrapegoodreads/cli.py
@@ -5,8 +5,10 @@ import time
 from pathlib import Path
 from typing import Optional
 
+import bleach
 import keyring
 import typer
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 
@@ -36,32 +38,77 @@ def _default_web_driver() -> BuiltinWebDriver:
 # --- Log-in ---
 
 
-def _login_password() -> str:
-    key_service_name = "goodreads-webscraping"
-    key_user = "user"
+def _login_password() -> Optional[str]:
+    key_service_name, key_user = "goodreads-webscraping", "user"
     pswd = keyring.get_password(key_service_name, key_user)
-    if pswd is None:
-        pswd = typer.prompt("password", hide_input=True)
-        keyring.set_password(key_service_name, key_user, pswd)
+    if pswd is not None:
+        return pswd
+
+    pswd = bleach.clean(input("password (leave blank if none): ")).strip()
+    if pswd is None or pswd == "":
+        return None
+
+    keyring.set_password(key_service_name, key_user, pswd)
     return pswd
+
+
+def _select_remember_me_button(driver: RemoteWebDriver) -> None:
+    remember_me_button = driver.find_element(By.ID, "remember_me")
+    if not remember_me_button.is_selected():
+        remember_me_button.click()
+    return None
+
+
+def _input_email(driver: RemoteWebDriver, email: str) -> None:
+    email_element = driver.find_element(By.ID, "user_email")
+    email_element.clear()
+    email_element.send_keys(email)
+    return None
+
+
+def _input_password(driver: RemoteWebDriver, password: str) -> None:
+    password_element = driver.find_element(By.ID, "user_password")
+    password_element.clear()
+    password_element.send_keys(password)
+    return None
+
+
+class NotLoggedInException(BaseException):
+    """Unsuccessful log-in."""
+
+    ...
 
 
 def goodreads_login(driver: RemoteWebDriver, email: str) -> None:
     driver.get(GOODREADS_URLS["sign-in"])
-    email_element = driver.find_element(By.ID, "user_email")
-    password_element = driver.find_element(By.ID, "user_password")
-    email_element.clear(), password_element.clear()
-    email_element.send_keys(email)
-    password_element.send_keys(_login_password())
-    remember_me_button = driver.find_element(By.ID, "remember_me")
-    if not remember_me_button.is_selected():
-        remember_me_button.click()
-    driver.find_element(By.NAME, "next").click()
-    # TODO: add method for checking if successfull.
+    _select_remember_me_button(driver)
+    _input_email(driver, email)
+    if (pswd := _login_password()) is not None:
+        _input_password(driver, pswd)
+        driver.find_element(By.NAME, "next").click()
+    else:
+        input("Sign in to Goodreads in the browser and hit 'Enter' when done: ")
     return None
 
 
 # --- Profile ---
+
+
+def _check_successful_login(driver: RemoteWebDriver) -> None:
+    if driver.current_url == GOODREADS_URLS["sign-in"]:
+        driver.quit()
+        logger.error("Currently at sign-in page.")
+        raise NotLoggedInException()
+    try:
+        driver.find_element(By.ID, "signIn")
+        driver.quit()
+        logger.error("Found sign-in box.")
+        raise NotLoggedInException()
+    except NoSuchElementException:
+        ...
+
+    logger.info("Successful log-in.")
+    return None
 
 
 @app.command()
@@ -87,8 +134,13 @@ def profile(
 
     web_driver = make_builtin_driver(gr_config.driver)
     goodreads_login(web_driver, email=gr_config.user_email)
+    _check_successful_login(web_driver)
+
+    # TODO: web-scraping...
+
     time.sleep(2)
     web_driver.quit()
+    logger.info("Goodreads web-scraping complete.")
     return None
 
 
